9bbb1ce815a72c4a8fd6c5b71490abb43b4910ca
mrsekut <k.cloudspider@gmail.com>
2019-09-27 01:29:17 +0900

    initial commit

---
+++ lib/issues_ex.ex
@@ -0,0 +1,18 @@
+defmodule IssuesEx do
+  @moduledoc """
+  Documentation for IssuesEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IssuesEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/issues_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule IssuesExTest do
+  use ExUnit.Case
+  doctest IssuesEx
+
+  test "greets the world" do
+    assert IssuesEx.hello() == :world
+  end
+end
